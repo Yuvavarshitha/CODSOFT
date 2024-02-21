ea024a3050d3a0d312766010be9f2086bb79cc50
Yuvavarshitha <yuvavarshithakb@gmail.com>
2024-02-21 19:41:32 +0530

    Cleaned up the code

--- ToDo_List.py
+++ ToDo_List.py
@@ -22,7 +22,7 @@ def addTask():
         #comlabel = tk.Label(info, text="task", font=("bold"))
         taskframe.destroy()
         update()
-
+    #function that executes when the current task is deleted
     def delTask():
         taskList.remove(task)
         info["Pending Tasks"] = len(taskList)
@@ -32,7 +32,8 @@ def addTask():
     task = newtaskEnt.get()
     taskList.append(task)
     newtaskEnt.delete(0,tk.END)
-
+    
+    #Creating a frame for the new task
     taskframe = tk.Frame(f2, relief=tk.RIDGE, borderwidth=3, width=100, bg="#252422")
     taskframe.pack(side=tk.TOP, fill="x") #fill='x' is used to utilize the entire available width left after allocating space for the widgets
 
@@ -46,7 +47,7 @@ def addTask():
     update()
 
 window = tk.Tk()
-
+window.geometry("800x400")
 #Organize the frames inside the window using the grid geometry manager
 #the window will have Two Frames
 window.rowconfigure(0, minsize=200, pad=2, weight=1)
@@ -56,16 +57,15 @@ window.columnconfigure([0,1], minsize=300, pad=2, weight=1)
 #frame that inputs a new task and adds it to task list and displays details
 f1 = tk.Frame(window, relief=tk.RIDGE, borderwidth=3, bg="#EB5E28")
 f1.grid(row=0, column=1, sticky="nsew")
-f1.rowconfigure(0,minsize=60, pad=2, weight=1)
-f1.columnconfigure([0,1], minsize=80, pad=2, weight=1)
+
 #APP NAME
-"""logoFrame = tk.Frame(f1, relief=tk.GROOVE, pady=10)
-logoFrame.grid(row=0, column=1, sticky="nsew")"""
-logo = tk.Label(f1, text="Task Manager", bg="#EB5E28", fg="white", font=("Fantasy",50,"italic"))
-logo.grid(row=0, column=1, sticky="nsew")
+logoFrame = tk.Frame(f1, relief=tk.GROOVE, pady=10, bg="#EB5E28")
+logoFrame.pack(side=tk.TOP, fill="both")
+logo = tk.Label(logoFrame, text="Task Manager", bg="#EB5E28", fg="white", font=("Fantasy",50,"italic"))
+logo.pack()
 #New task entry section
 inputFrame = tk.Frame(f1, relief=tk.GROOVE, pady=10)
-inputFrame.grid(row=1, column=0, sticky="nsew")
+inputFrame.pack(side=tk.TOP, fill="both")
 
 new_taskLbl = tk.Label(inputFrame, text="Enter a task", fg="#252422", font=("bold"))
 new_taskLbl.pack(side=tk.LEFT, padx=10)
@@ -78,13 +78,11 @@ add_Task_Btn.pack(side=tk.LEFT, padx=10)
 
 #Additional Frame that contains info
 infoFrame =  tk.Frame(f1, relief=tk.GROOVE)
-infoFrame.grid(row=1, column=1, sticky="nsew")
+infoFrame.pack(side=tk.TOP, fill="both")
 information = f"Pending Tasks: \t{info['Pending Tasks']} \nCompleted Tasks: {info['Completed Tasks']}" 
 msg = tk.Message(infoFrame, text=information)
 msg.pack(side=tk.TOP)
-"""
-showBtn = tk.Button(infoFrame, text="Show Details", bg="#CCC5B9", fg="#252422", command=show)
-showBtn.pack(side=tk.TOP)"""
+
 
 #FRAME2
 #frame to that contains task list
