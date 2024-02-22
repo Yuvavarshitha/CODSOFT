51e1a4ad6824faac58879ad2ee9dee1f276d351a
Yuvavarshitha <yuvavarshithakb@gmail.com>
2024-02-22 21:29:08 +0530

    Added a priority based task input functionality

--- ToDo_List.py
+++ ToDo_List.py
@@ -10,7 +10,7 @@ info = {"Pending Tasks":len(taskList),"Completed Tasks":len(completedTasks) }
 def update():
     info["Pending Tasks"] = len(taskList)
     info["Completed Tasks"] = len(completedTasks)
-    msg.configure(text= f"Pending Tasks: \t{info['Pending Tasks']} \nCompleted Tasks: {info['Completed Tasks']}")
+    msg.configure(text= f"\n\nPending Tasks:   {info['Pending Tasks']} \nCompleted Tasks:   {info['Completed Tasks']}")
 
 def addTask():
     #function that executes when the current task is completed 
@@ -34,8 +34,8 @@ def addTask():
     newtaskEnt.delete(0,tk.END)
     
     #Creating a frame for the new task
-    taskframe = tk.Frame(f2, relief=tk.RIDGE, borderwidth=3, width=100, bg="#252422")
-    taskframe.pack(side=tk.TOP, fill="x") #fill='x' is used to utilize the entire available width left after allocating space for the widgets
+    
+    taskframe = tk.Frame(tasks, relief=tk.RIDGE, borderwidth=3, width=100, bg="#252422")
 
     new_task = tk.Label(taskframe, text=task, fg="white", bg="#252422", font=("bold"))
     new_task.pack(side=tk.LEFT, fill="x")
@@ -44,6 +44,12 @@ def addTask():
     del_btn.pack(side=tk.RIGHT)
     com_btn = tk.Button(taskframe, text="Complete", bg="green", fg="black", command=comTask)
     com_btn.pack(side=tk.RIGHT)
+
+    if str(priority).strip() == "high":
+        taskframe.pack(side=tk.TOP, fill="x")
+    else:
+        taskframe.pack(side=tk.BOTTOM, fill="x") #fill='x' is used to utilize the entire available width left after allocating space for the widgets
+
     update()
 
 window = tk.Tk()
@@ -61,7 +67,7 @@ f1.grid(row=0, column=1, sticky="nsew")
 #APP NAME
 logoFrame = tk.Frame(f1, relief=tk.GROOVE, pady=10, bg="#EB5E28")
 logoFrame.pack(side=tk.TOP, fill="both")
-logo = tk.Label(logoFrame, text="Task Manager", bg="#EB5E28", fg="white", font=("Fantasy",50,"italic"))
+logo = tk.Label(logoFrame, text="Task Manager", bg="#EB5E28", fg="white", font=("Fantasy",40,"italic"))
 logo.pack()
 #New task entry section
 inputFrame = tk.Frame(f1, relief=tk.GROOVE, pady=10)
@@ -73,14 +79,29 @@ new_taskLbl.pack(side=tk.LEFT, padx=10)
 newtaskEnt = tk.Entry(inputFrame)
 newtaskEnt.pack(side=tk.LEFT, padx=10)
 
-add_Task_Btn = tk.Button(inputFrame, text="Add Task", bg="#CCC5B9", fg="#252422", command=addTask)
-add_Task_Btn.pack(side=tk.LEFT, padx=10)
+#Adding Priority to each task
+inputFrame2 = tk.Frame(f1, relief=tk.GROOVE, pady=10)
+inputFrame2.pack(side=tk.TOP, fill="both")
+
+priority = StringVar()
+priority.set("Normal ")
+options = ["Normal ", "Medium", "  High  "]
+priorityLabel = tk.Label(inputFrame2, text="Select Priority", fg="#252422", font=("bold"))
+priorityLabel.pack(side=tk.LEFT, padx=10)
+
+priorityInp = OptionMenu(inputFrame2, priority, *options)
+priorityInp.pack(side=tk.LEFT, padx=10)
+
+#Add Task Button
+add_Task_Btn = tk.Button(inputFrame2, text="Add Task", bg="#CCC5B9", fg="#252422", command=addTask)
+add_Task_Btn.pack(side=tk.LEFT, padx=20)
+
 
 #Additional Frame that contains info
 infoFrame =  tk.Frame(f1, relief=tk.GROOVE)
 infoFrame.pack(side=tk.TOP, fill="both")
-information = f"Pending Tasks: \t{info['Pending Tasks']} \nCompleted Tasks: {info['Completed Tasks']}" 
-msg = tk.Message(infoFrame, text=information)
+information = f"\n\nPending Tasks:   {info['Pending Tasks']} \n\nCompleted Tasks:   {info['Completed Tasks']}" 
+msg = tk.Message(infoFrame, text=information, width=150)
 msg.pack(side=tk.TOP)
 
 
@@ -93,5 +114,8 @@ taskframe = tk.Frame(f2, relief=tk.RIDGE, borderwidth=3)
 taskframe.pack(side=tk.TOP, fill="x")
 label = tk.Label(taskframe, text="Tasks TO DO", bg="#252422", fg="white", font=("Gill Sans",30,"bold"))
 label.pack(side=tk.TOP, fill="x")
+
+tasks = tk.Frame(taskframe)
+tasks.pack(side=tk.TOP, fill="x")
 
 window.mainloop()
